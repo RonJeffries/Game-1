4f5323c8aa3f5e85f167c598c191cc211a77c51c
Ron Jeffries <ronjeffries@acm.org>
2020-03-07 09:52:37 -0500

    minor changes pre braitenbugs

--- Main.lua
+++ Main.lua
@@ -2,6 +2,7 @@
 -- RJ 20200222 initial plan, make a floor
 -- RJ 20200226 camera object works w/o touch
 -- RJ 20200227 cleanup
+-- RJ 20200307 clean a bit for bugs
 
 function setup()
     scene = craft.scene()
@@ -19,7 +20,7 @@ function setup()
     scene.voxels:resize(vec3(5,1,5))      
     scene.voxels.coordinates = vec3(-16*m,0,-16*m)    
     scene.voxels:fill("Dirt Grass")
-    scene.voxels:box(0,2,0,16*m,2,16*m)
+    scene.voxels:box(0,1,0,16*m,1,16*m)
     scene.voxels:fill("Bedrock")
     scene.voxels:box(0,0,0, 16*m,0,16*m)
     
