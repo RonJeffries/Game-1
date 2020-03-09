b563cc8c2d136fd6e4b38d5819e20b410b1b9fa7
Ron Jeffries <ronjeffries@acm.org>
2020-03-09 11:09:32 -0400

    rotates and moves correctly
    Have to update his direction on change

--- Creature.lua
+++ Creature.lua
@@ -7,6 +7,7 @@ function Creature:init(entity, x, z, behaviors)
     self.behaviors = behaviors or {self.walk}
     self.angle = 0
     self.time = 0
+    self.move = vec2(0,1)
     entity.x = x
     entity.z = z
     entity.y = 2 -- just above the ground
@@ -24,15 +25,19 @@ end
 function Creature:walk()
     self.time = self.time + 1
     local speed = 100
-    if self.time == 50 then
+    if self.time == 200 then
         self.time = 0
-        self.angle = math.random()*90 - 45
+        if math.random() < 0.5 then
+            self.angle = 45
+        else
+            self.angle = -45
+        end
         local rotor = quat.eulerAngles(0,-self.angle,0)
         self.entity.rotation = self.entity.rotation*rotor
+        self.move = self.move:rotate(math.rad(self.angle))
     end
-    local move = vec2(0,1):rotate(math.rad(self.angle))
-    self.entity.x = self.entity.x + move.x/speed
-    self.entity.z = self.entity.z + move.y/speed
+    self.entity.x = self.entity.x + self.move.x/speed
+    self.entity.z = self.entity.z + self.move.y/speed
 end
 
 
