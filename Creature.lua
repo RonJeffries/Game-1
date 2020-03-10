771456e3628d8e3a1816d7dda9d07bc35d018cae
Ron Jeffries <ronjeffries@acm.org>
2020-03-10 11:05:20 -0400

    Clean up behavior to return table of speed and turn

--- Creature.lua
+++ Creature.lua
@@ -5,9 +5,7 @@ local CreatureMaxTime = 300
 function Creature:init(entity, x, z, behaviors)
     self.entity = entity
     self.behaviors = behaviors or {self.walk}
-    self.angle = 0
     self.time = 0
-    self.move = vec2(0,1)
     entity.x = x
     entity.z = z
     entity.y = 2 -- just above the ground
@@ -16,28 +14,31 @@ function Creature:init(entity, x, z, behaviors)
 end
 
 function Creature:update(dt)
-    local dt = dt or DeltaTime
+    local divisor = 100
+    local speed = 0
+    local turn = 0
     for _,b in pairs(self.behaviors) do
-        b(self)
+        local tab = b(self)
+        speed = speed + (tab.speed or 0)
+        turn = turn + (tab.turn or 0)
     end
+    local rotor = quat.eulerAngles(0,turn,0)
+    self.entity.rotation = self.entity.rotation*rotor
+    local move = self.entity.forward
+    self.entity.position = self.entity.position + move*speed/divisor
 end
 
 function Creature:walk()
+    local tab = {speed=1}
     self.time = self.time + 1
-    local speed = 100
-    if self.time == 200 then
+    local speed = 200
+    if self.time == 100 then
         self.time = 0
         if math.random() < 0.5 then
-            self.angle = 45
+            tab.turn = 45
         else
-            self.angle = -45
+            tab.turn = -45
         end
-        local rotor = quat.eulerAngles(0,-self.angle,0)
-        self.entity.rotation = self.entity.rotation*rotor
-        self.move = self.move:rotate(math.rad(self.angle))
     end
-    self.entity.x = self.entity.x + self.move.x/speed
-    self.entity.z = self.entity.z + self.move.y/speed
+    return tab
 end
-
-
