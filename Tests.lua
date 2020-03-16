c24d40ad53c63235deee3c1cf8e7e70295e4ae44
Ron Jeffries <ronjeffries@acm.org>
2020-03-16 16:10:04 -0400

    Linked in CodeaUnit and Tests.lua

---
+++ Tests.lua
@@ -0,0 +1,19 @@
+function testGame1()
+    CodeaUnit.detailed = true
+
+    _:describe("Game1 Tests", function()
+
+        _:before(function()
+            -- Some setup
+        end)
+
+        _:after(function()
+            -- Some teardown
+        end)
+        
+        _:test("Try again", function()
+            _:expect( 2+1).is(3)
+        end)
+
+    end)
+end
