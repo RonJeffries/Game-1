09c90d0de282c7724a0def332c78702e561da636
Ron Jeffries <ronjeffries@acm.org>
2020-03-28 11:17:41 -0400

    Angle between test

--- TestGame.lua
+++ TestGame.lua
@@ -17,6 +17,17 @@ function testGame1()
         _:test("Try again", function()
             _:expect(2+1).is(3)
         end)
+        
+        _:test("Angle Between", function()
+            local v1 = vec2(0,0)
+            local v2 = vec2(1,1)
+            local a1 = vec2(1,0):angleBetween(v2)
+            local aDeg = math.deg(a1)
+            _:expect(aDeg).is(45)
+            local a2 = vec2(0,1):angleBetween(v2)
+            local a2Deg = math.deg(a2)
+            _:expect(a2Deg).is(-45)
+        end)
 
     end)
 end
